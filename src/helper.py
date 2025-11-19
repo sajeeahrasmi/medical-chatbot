2e2b8b6c1750f4fe5969d1acea9e7b0089ad5727
sajeeahrasmi <sajeeahrasmi@gmail.com>
2025-11-19 21:43:02 +0530

    done creating

---
+++ src/helper.py
@@ -0,0 +1,44 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+#Extract data from PDF files
+def load_pdf_file(data):
+    loader= DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+    documents = loader.load()
+    return documents
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs : List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source": src}
+            )
+        )
+    return minimal_docs
+
+# split the documents into smaller chunks
+
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+    )
+    texts_chunk = text_splitter.split_documents(minimal_docs)
+    return texts_chunk
+
+# download the embeddings from HuggingFace
+def download_hugging_face_embeddings():
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  
+    return embeddings
+
+
